05b36976c62903fa9bc7800e803d85add65ba553
Vipul <vipulraj.156@gmail.com>
2026-04-09 18:55:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot is alive!"
+
+def run():
+  app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# Sabse pehle server start karein
+keep_alive()
+
+# Yahan se aapka asli Telegram bot ka code shuru hoga...
 print("EOL: exiting..")
